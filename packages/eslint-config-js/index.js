9f22dfb1d5df1575b23de2b28e1498f571f3d27f
Gabriel Melo <gabriel.smelo@proton.me>
2023-03-25 21:57:32 -0300

    remove prettier extends

--- packages/eslint-config-js/index.js
+++ packages/eslint-config-js/index.js
@@ -1,17 +1,24 @@
+const prettier = require('../prettier-config')
+
 module.exports = {
 	env: {
 		browser: true,
 		es2021: true,
 		node: true,
 	},
-	extends: ['eslint:recommended', 'prettier', 'plugin:prettier/recommended'],
+	extends: ['eslint:recommended', 'plugin:prettier/recommended'],
 	overrides: [],
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
 	},
 	rules: {
-		'prettier/prettier': 'error',
+		'prettier/prettier': [
+			'error',
+			{
+				...prettier,
+			},
+		],
 		'arrow-body-style': 'off',
 		'prefer-arrow-callback': 'off',
 	},
--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  arrowParens: "always",
-  bracketSpacing: true,
-  bracketSameLine: false,
-  printWidth: 80,
-  useTabs: true,
-  tabWidth: 2,
-  semi: false,
-  singleQuote: true,
-};
+	arrowParens: 'always',
+	bracketSpacing: true,
+	bracketSameLine: false,
+	printWidth: 80,
+	useTabs: true,
+	tabWidth: 2,
+	semi: false,
+	singleQuote: true,
+}
